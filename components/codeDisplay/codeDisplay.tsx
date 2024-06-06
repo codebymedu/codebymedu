faa9b19fd46c47a500bf751d61a824ac37caf5af
Medu <03medu@gmail.com>
2024-06-07 01:02:47 +0200

    wip

--- components/codeDisplay/codeDisplay.tsx
+++ components/codeDisplay/codeDisplay.tsx
@@ -11,15 +11,33 @@ const CodeDisplay = ({ children }: CodeDisplayProps) => {
     <Card className="w-full py-4 shadow-[0px_-8px_3000px_0px_#b249f8] ">
       <CardBody className=" items-center">{children}</CardBody>
 
-      <CardBody className="">
-        <pre>
-          {`
-          <html>test</html>
-          `}
-        </pre>
+      <CardBody className="px-16">
+        <TagDisplay tag="about-me">
+          <TagDisplay tag="name">Meduard Krasniqi (Medu)</TagDisplay>
+
+          <TagDisplay tag="focus">Frontend Development</TagDisplay>
+
+          <TagDisplay tag="skills">
+            Building intuitive and dynamic web applications
+          </TagDisplay>
+
+          <TagDisplay tag="experience">Frontend Technical Lead</TagDisplay>
+        </TagDisplay>
       </CardBody>
     </Card>
   );
 };
 
 export { CodeDisplay, CodeDisplayTab };
+
+type TagDisplayProps = { children: ReactNode; tag: string };
+
+const TagDisplay = ({ children, tag }: TagDisplayProps) => (
+  <div className="pl-4 my-2">
+    <p className="text-violet-500">{`<${tag}>`}</p>
+
+    <div className="pl-4">{children}</div>
+
+    <p className="text-violet-500">{`</${tag}>`}</p>
+  </div>
+);
