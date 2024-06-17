8709dcfadd405fc4f1f177a6a66dec98f717ecc3
Medu <03medu@gmail.com>
2024-06-17 13:45:30 +0200

    feat: show card of tools/deliverables

--- components/horizontalRoadmap/horizontalRoadmap.tsx
+++ components/horizontalRoadmap/horizontalRoadmap.tsx
@@ -1,17 +1,11 @@
 "use client";
 
-import {
-  ArrowPathIcon,
-  ChatBubbleBottomCenterTextIcon,
-  CheckCircleIcon,
-  CogIcon,
-  RocketLaunchIcon,
-} from "@heroicons/react/20/solid";
 import clsx from "clsx";
 import { getOrdinalSuffix } from "@codebymedu/utils/helpers";
 import { useState } from "react";
-import { Chip, Progress, Slider } from "@nextui-org/react";
+import { Progress } from "@nextui-org/react";
 import { subtitle } from "@codebymedu/components/primitives";
+import { Card, CardBody } from "@nextui-org/react";
 
 const steps = [
   {
@@ -19,35 +13,68 @@ const steps = [
     title: "I Listen to You",
     description:
       "I start by understanding your vision. We discuss your ideas, goals, and needs. Your input is crucial to shape the project from the ground up.",
-    Icon: ChatBubbleBottomCenterTextIcon,
+    tools: ["MS Teams/Meets", "Email", "Notion"],
+    deliverables: [
+      "Project Requirements Document",
+      "Meeting Notes",
+      "Goals Summary",
+    ],
   },
   {
     id: 2,
     title: "We Plan and Build Together",
     description:
       "We create a clear plan and I use top technologies like Next.js, React, and Typescript to turn designs into working web applications. Your feedback helps at each stage.",
-    Icon: CogIcon,
+    tools: [
+      "Next.js/React",
+      "Typescript",
+      "ClickUp or similar",
+      "... and more",
+    ],
+    deliverables: [
+      "Project Plan",
+      "Development Roadmap",
+      "Wireframes",
+      "Initial Codebase",
+    ],
   },
   {
     id: 3,
-    title: "We Ensure Quality",
+    title: "I Ensure Quality",
     description:
-      "Through Code Reviews and Continuous Integration, I keep the code high-quality and efficient. Your involvement in reviews ensures the project meets your standards.",
-    Icon: CheckCircleIcon,
+      "Through Code Reviews and Continuous Integration, I keep the code high-quality and efficient. Automated tests and linters help maintain standards.",
+    tools: ["Jest", "Cypress", "ESLint", "Prettier", "GitHub or similar"],
+    deliverables: [
+      "Automated tests",
+      "CI/CD Pipeline",
+      "Test Reports",
+      "Linting Reports",
+    ],
   },
   {
     id: 4,
     title: "We Test and Launch Together",
     description:
       "I carefully test the application to find and fix any issues. Once everything is perfect, I handle the deployment process and ensure a smooth launch.",
-    Icon: RocketLaunchIcon,
+    tools: ["Docker", "AWS, Vercel or similar", "GitHub Actions"],
+    deliverables: [
+      "Deployment Scripts",
+      "Live Application",
+      "Monitoring Setup",
+    ],
   },
   {
     id: 5,
     title: "I Grow with You",
     description:
       "After launch, I stay in touch to gather user feedback and make necessary updates. Our collaboration continues to ensure the product evolves with your needs.",
-    Icon: ArrowPathIcon,
+    tools: ["Google Analytics", "Hotjar", "GitHub", "Slack"],
+    deliverables: [
+      "User Feedback Reports",
+      "Analytics Dashboards",
+      "Regular Updates",
+      "Support Documentation",
+    ],
   },
 ];
 
@@ -99,10 +126,19 @@ export const HorizontalRoadmap = () => {
               <div className="flex gap-4 items-start">
                 <div className="h-2 w-2 bg-violet-700 rounded-full"></div>
 
-                <p className="pt-0 -mt-2 text-white">{step.title}</p>
+                <p
+                  className={clsx(
+                    "pt-0 -mt-2  ",
+                    step.id === activeStepId
+                      ? "text-white"
+                      : "text-black dark:text-white"
+                  )}
+                >
+                  {step.title}
+                </p>
               </div>
 
-              <p className="text-xs -mt-1 text-white">{step.id}</p>
+              <p className="text-xs -mt-1 text-white ">{step.id}</p>
             </div>
 
             <Progress
@@ -117,15 +153,35 @@ export const HorizontalRoadmap = () => {
         ))}
       </div>
 
-      <div className="mt-8 p-2 w-96">
-        <h3 className={subtitle()}>
-          {activeStep.id}
-          {getOrdinalSuffix(activeStep.id)} Step: {activeStep.title}
-        </h3>
+      <div className="mt-8 p-2 flex justify-between">
+        <div className=" w-96">
+          <h3 className={subtitle()}>
+            {activeStep.id}
+            {getOrdinalSuffix(activeStep.id)} Step: {activeStep.title}
+          </h3>
 
-        <p>{activeStep.description}</p>
+          <p>{activeStep.description}</p>
+        </div>
 
-        {/* TODO: Right side: Deliverables, tools, etc. */}
+        <Card className="w-72 h-96">
+          <CardBody className="p-6">
+            <h2 className="text-lg font-semibold mb-4">Deliverables</h2>
+
+            <ul className="list-disc pl-4 mb-6">
+              {activeStep.deliverables.map((deliverable) => (
+                <li>{deliverable}</li>
+              ))}
+            </ul>
+
+            <h2 className="text-lg font-semibold mb-4">Tools</h2>
+
+            <ul className="list-disc pl-4 ">
+              {activeStep.tools.map((tool) => (
+                <li>{tool}</li>
+              ))}
+            </ul>
+          </CardBody>
+        </Card>
       </div>
     </div>
   );
